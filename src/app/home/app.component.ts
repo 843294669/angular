a7ee8ab8cf7547b08e29bfa76eb6b95776325688
独孤九剑 <843294669@qq.com>
2023-11-28 13:57:48 +0800

    Imports DemoComponent.

--- src/app/home/app.component.ts
+++ src/app/home/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DemoComponent } from '../demo/demo.component';
 
 // metadata defines the properties
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, DemoComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
