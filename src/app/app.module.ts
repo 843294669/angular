d3516100c8041f7958da07455286e249a01c0572
独孤九剑 <843294669@qq.com>
2023-11-28 13:00:08 +0800

    Add module

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+// import { BrowserModule } from '@angular/platform-browser'; 
+// import { DemoComponent } from './demo/demo.component';
+// import { AppComponent } from './home/app.component';
+
+// @NgModule({
+//     declarations: [DemoComponent, AppComponent],
+//     imports: [CommonModule, BrowserModule, RouterOutlet],
+//     providers: [],
+//     //exports: [DemoComponent],
+//     bootstrap: [AppComponent]
+// })
+
+// export class AppModule {
+// }
