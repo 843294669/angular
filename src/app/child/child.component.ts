d7a7d3aa74a9b957a6612b2ed7a0fa81493d4536
独孤九剑 <843294669@qq.com>
2023-11-28 17:31:27 +0800

    接收路由参数。

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms'; 
+import { ActivatedRoute } from '@angular/router';
 
 // metadata defines the properties
 @Component({
@@ -13,9 +14,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './child.component.scss'
 })
 export class ChildComponent {
-  value: String = "jacob";
+  value: String = "";
   changeValue: String = "";
   onChange = () => {
     this.changeValue = this.value;
   }
+  ngOnInit() {
+    // query params
+    // this.title = this.route.snapshot.queryParams["title"];
+    // router path params
+    // this.title = this.route.snapshot.params["title"];
+    // query params
+    this.route.queryParams.subscribe({
+      next: (v) => {console.log(v['title']); this.value = v['title']},
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
+  }
+  constructor(public route: ActivatedRoute) { }
 }
