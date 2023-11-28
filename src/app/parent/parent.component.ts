f39129673dedc7dd81e966839aae4e52235d4fe2
独孤九剑 <843294669@qq.com>
2023-11-28 17:31:08 +0800

    路由传参。

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { RouterLinkActive } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ChildComponent } from '../child/child.component';
 
 // metadata defines the properties
 @Component({
-  selector: 'app-root',
+  selector: 'parent',
   standalone: true,
-  imports: [CommonModule, ChildComponent, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [CommonModule, ChildComponent, RouterOutlet],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.scss'
 })
+// 构造函数中实例化
+
 export class ParentComponent {
+  title: String = '';
+  ngOnInit() {
+    // query params
+    // this.title = this.route.snapshot.queryParams["title"];
+    // router path params
+    // this.title = this.route.snapshot.params["title"];
+    // router path params
+    this.route.params.subscribe({
+      next: (v) => {console.log(v['title']); this.title = v['title']},
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
+  }
+  constructor(public route: ActivatedRoute) { }
 }
